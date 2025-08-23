b62da8a225b7082d27d1ef93f4751d5f1fcbec30
sutanukaa <sutanukachakraborty004@gmail.com>
2025-08-23 23:46:58 +0530

    Midlle commit

--- src/components/landing-page/index.ts
+++ src/components/landing-page/index.ts
@@ -1,6 +1,6 @@
-export { default as LandingPage } from './landingpage';
-export { CompanyMarquee } from './components/CompanyMarquee';
+export { default as CampusPlacementHero } from './CampusPlacementHero';
+export { MarqueeDemoVertical } from './components/Scrolls';
 export { default as GradualSpacing } from './components/GradualSpacing';
-export { default as NavigationBar } from './components/NavigationBar';
+export { default as PillNav } from './components/PillNav';
 export { default as AboutSection } from './components/AboutSection';
-export { default as FeaturesSection } from './components/FeaturesSection';
+export { TopLeftShiningLight, TopRightShiningLight } from './svgs/ShinyLighs';
