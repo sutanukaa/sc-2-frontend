14b5d8b1f910e692507ef1c4fb78eb9e466e43e6
sutanukaa <sutanukachakraborty004@gmail.com>
2025-08-23 13:25:49 +0530

    hero-added

---
+++ src/components/landing-page/index.ts
@@ -0,0 +1,5 @@
+export { default as CampusPlacementHero } from './CampusPlacementHero';
+export { MarqueeDemoVertical } from './components/Scrolls';
+export { default as GradualSpacing } from './components/GradualSpacing';
+export { default as PillNav } from './components/PillNav';
+export { TopLeftShiningLight, TopRightShiningLight } from './svgs/ShinyLighs';
